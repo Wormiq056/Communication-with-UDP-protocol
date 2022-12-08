0d20357e4036b1a21b59a6e2689d0804d3580e11
Wormiq056 <matrus001@gmail.com>
2022-12-08 16:31:51 +0100

    fix lowest fragment size

--- sender/client.py
+++ sender/client.py
@@ -281,7 +281,7 @@ class Client:
         while True:
             fragment_size = input(
                 f"[INPUT] What will be the fragment size?(default/max = {PROTOCOL_SIZE - HEADER_SIZE} bytes,"
-                f" lowest = {LOWEST_FRAGMENT_SIZE} bytes): ") + HEADER_SIZE
+                f" lowest = {LOWEST_FRAGMENT_SIZE} bytes): ")
             if fragment_size == "":
                 fragment_size = PROTOCOL_SIZE
                 break
@@ -293,7 +293,7 @@ class Client:
             if msg == "":
                 self.simulate_error(TXT)
             else:
-                packets = PacketFactory.create_txt_packets(fragment_size, msg)
+                packets = PacketFactory.create_txt_packets(int(fragment_size) + HEADER_SIZE, msg)
                 self.start_transfer(packets)
         else:
             while True:
@@ -304,7 +304,7 @@ class Client:
             if file_path == "":
                 self.simulate_error(FILE)
             else:
-                packets = PacketFactory.create_file_packets(file_path, int(fragment_size))
+                packets = PacketFactory.create_file_packets(file_path, int(fragment_size) + HEADER_SIZE)
                 self.start_transfer(packets)
 
     def simulate_error(self, test_type: bytes) -> None:
